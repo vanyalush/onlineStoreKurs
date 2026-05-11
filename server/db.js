7446f5bd977e53fea81bad4a5edd1783534d1c4d
rylllin <148778441+vanyalush@users.noreply.github.com>
2026-05-11 23:09:11 +0400

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,20 +1,13 @@
 require('dotenv').config();
 const {Sequelize} = require('sequelize');
+const url = "postgresql://admin:IjTKV2d1E4prUpEbxY3qTkxCD20e9Arm@dpg-d81214l7vvec73enamjg-a.virginia-postgres.render.com/online_store_mgge";
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        logging: false,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
+module.exports = new Sequelize(url, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
         }
     }
-);
+});
